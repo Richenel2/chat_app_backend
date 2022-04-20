9db32bfb27bf5db2e26d191232f035981b08ebf0
Richenel2 <yvanprogramme@gmail.com>
2022-04-20 02:20:45 +0100

    modif

--- chat/consumer.py
+++ chat/consumer.py
@@ -1,13 +1,14 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from .models import Message
 from .serializers import MessageSerializer
+from asgiref.sync import sync_to_async
 
 class MobileConsumer(AsyncJsonWebsocketConsumer):
     
         async def connect(self):
             await self.channel_layer.group_add("mobile",self.channel_name)
             await self.accept()
-            list = await map( lambda x: MessageSerializer(x).data,Message.objects.all().order_by('creation_date')[:20])
+            list = sync_to_async(map( lambda x: MessageSerializer(x).data,Message.objects.all().order_by('creation_date')[:20]))
             await self.send_json(list)
 
 
@@ -21,7 +22,7 @@ class MobileConsumer(AsyncJsonWebsocketConsumer):
            )      
     
         async def message(self,event):
-          list = await map( lambda x: MessageSerializer(x).data,Message.objects.all().order_by('creation_date')[:20])
+          list = sync_to_async(map( lambda x: MessageSerializer(x).data,Message.objects.all().order_by('creation_date')[:20]))
           await self.send_json(list)
         
   
